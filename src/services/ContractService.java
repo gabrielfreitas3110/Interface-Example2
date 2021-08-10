d933820ec47569fe1fa485cf85a98fae4fdf5bd8
Gariel Augusto <gabriel.freitas3110@gmail.com>
2021-08-10 11:27:41 -0300

    A example of interface implementation in a Contract Service

---
+++ src/services/ContractService.java
@@ -0,0 +1,35 @@
+package services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import entities.Contract;
+import entities.Installment;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, int months) {
+		double basicQuota = contract.getTotalValue() / months;
+		
+		for(int i=1; i<=months; i++) {
+			double updatedQuota = basicQuota + onlinePaymentService.interest(basicQuota, i);
+			double totalQuota = updatedQuota + onlinePaymentService.paymentFee(updatedQuota);
+			Date dueDate = addMonths(contract.getDate(),i);
+			contract.getInstallments().add(new Installment(dueDate,totalQuota));
+		}
+		
+	}
+	
+	private Date addMonths(Date date, int n) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		calendar.add(Calendar.MONTH, n);
+		return calendar.getTime();
+	}
+}
